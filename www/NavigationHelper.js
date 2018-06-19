558d89dc6572f4ab6ec12520a7e971999f590ed3
Dante Lee <35988175+lwz467666145@users.noreply.github.com>
2018-06-19 19:42:20 +0800

    Update NavigationHelper.js

--- www/NavigationHelper.js
+++ www/NavigationHelper.js
@@ -4,34 +4,34 @@ exports.baiduCheck = function (success) {
     exec(success, null, 'NavigationHelper', 'baiduCheck', []);
 };
 
-exports.baiduAddress = function (address, success, error) {
-    exec(success, error, 'NavigationHelper', 'baiduAddress', [address]);
+exports.baiduAddress = function (address, error) {
+    exec(null, error, 'NavigationHelper', 'baiduAddress', [address]);
 };
 
-exports.baiduPosition = function (longitude, latitude, success, error) {
-    exec(success, error, 'NavigationHelper', 'baiduPosition', [longitude, latitude]);
+exports.baiduPosition = function (longitude, latitude, error) {
+    exec(null, error, 'NavigationHelper', 'baiduPosition', [longitude, latitude]);
 };
 
 exports.gaodeCheck = function (success) {
     exec(success, null, 'NavigationHelper', 'gaodeCheck', []);
 };
 
-exports.gaodeAddress = function (address, success, error) {
-    exec(success, error, 'NavigationHelper', 'gaodeAddress', [address]);
+exports.gaodeAddress = function (address, error) {
+    exec(null, error, 'NavigationHelper', 'gaodeAddress', [address]);
 };
 
-exports.gaodePosition = function (name, longitude, latitude, success, error) {
-    exec(success, error, 'NavigationHelper', 'gaodePosition', [name, longitude, latitude]);
+exports.gaodePosition = function (name, longitude, latitude, error) {
+    exec(null, error, 'NavigationHelper', 'gaodePosition', [name, longitude, latitude]);
 };
 
 exports.tencentCheck = function (success) {
     exec(success, null, 'NavigationHelper', 'tencentCheck', []);
 };
 
-exports.tencentAddress = function (address, success, error) {
-    exec(success, error, 'NavigationHelper', 'tencentAddress', [address]);
+exports.tencentAddress = function (address, error) {
+    exec(null, error, 'NavigationHelper', 'tencentAddress', [address]);
 };
 
-exports.tencentPosition = function (longitude, latitude, success, error) {
-    exec(success, error, 'NavigationHelper', 'tencentPosition', [longitude, latitude]);
+exports.tencentPosition = function (longitude, latitude, error) {
+    exec(null, error, 'NavigationHelper', 'tencentPosition', [longitude, latitude]);
 };
